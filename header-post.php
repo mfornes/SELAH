8599d81c33cd7da2273f33deded1cc196f79fc87
Josue Aguilera Santos <josueaguilerasantos93@gmail.com>
2019-03-19 09:17:14 +0100

    Dimension del logo y cambiada la clase css del headre-content

--- header-post.php
+++ header-post.php
@@ -17,7 +17,7 @@
 					$custom_logo_id = get_theme_mod( 'custom_logo' );
 					$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 					if ( has_custom_logo() ) {
-						echo '<a href="'. esc_url( home_url( '/' ) ) .'" class="logo "><img class="" src="'. esc_url( $logo[0] ) .'" alt="Home"/></a>';
+						echo '<a href="'. esc_url( home_url( '/' ) ) .'" class="logo "><img class="" src="'. esc_url( $logo[0] ) .'" alt="Home"  width="165" height="50"/></a>';
 					} else {
 						echo '<a href="'. esc_url( home_url( '/' ) ) .'" class="logo"><h1>'. get_bloginfo( 'name' ) .'</h1></a>';
 					}
@@ -37,8 +37,8 @@
 						<li><a href="" class="icon-link"><img src="<?php echo get_template_directory_uri() . '/assets/images/Recurso 69.png'; ?>" alt="Search"/></a></li>
 					</ul>
 				</nav>
-                <div class="header-content" style="  background: left 0% top 80% url(<?php the_post_thumbnail_url() ?>)">
-                     <h1 class="faculty"><?php the_title();?></h1>
+                <div class="header-content-post" style="  background: left 0% top 80% url(<?php the_post_thumbnail_url() ?>)">
+                     <h1>News</h1>
                 </div>
   			</header>
 			  <main>
