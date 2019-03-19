56ee62cede7ca8d315e636a3fc0ba46603c1dd51
Josue Aguilera Santos <josueaguilerasantos93@gmail.com>
2019-03-19 09:19:13 +0100

    Titulo dinamico

--- single-post.php
+++ single-post.php
@@ -1,6 +1,6 @@
 <?php get_header( 'post' )?>
 <section class="content">
-      <h1>Post Information</h1>
+      <h1><?php the_title();?></h1>
       <span>19 Marzo, 2019</span>
       
       <div class="content-page">
@@ -8,6 +8,7 @@
       <?php while ( have_posts() ) : the_post();?>
             <p> <?php the_content()?></p>
       <?php  endwhile; ?>
+        <div style="clear: both"></div>
       </div>      
       
     </section> 
